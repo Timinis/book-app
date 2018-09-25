c13862f3a8801895994f42506b348223c882bbc1
dhwang21 <hwang.derrick@gmail.com>
2018-09-25 10:32:02 -0700

    adding google search api

--- server.js
+++ server.js
@@ -17,15 +17,29 @@ app.set('view engine', 'ejs');
 
 //API Routes
 app.get('/', newSearch);
+app.get('/')
 
-//Creates a new search to the Google Books API
-// app.post('/searches', createSearch);
+//creates a new search to the Google Books API
+app.post('/searches', createSearch);
+
+//Catch-all
 app.get('*', (request, response) =>
   response.status(404).send('This route does not exist')
 );
 
 function newSearch(request, response) {
-  response.render('index');
+  response.render('pages/index');
+  response.render('pages/searches/show');
 }
 
+function createSearch(request, response){
+    let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+    console.log(request.body);
+    if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
+    if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
+
+    superagent.get(url)
+        .then(apiResponse => console.log(apiResponse));
+    
+}
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
