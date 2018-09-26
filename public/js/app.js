3a0c040e05e310f99698e1d2f51846775b644663
dhwang21 <hwang.derrick@gmail.com>
2018-09-26 10:57:36 -0700

    finished adding toggle show and hide

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+const showHide = () => {
+    let x = document.getElementById("form-toggle");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+const showHideDetail = () => {
+    let x = document.getElementById("detail-toggle");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
