dcad6f28ec38e4476472a9e10eb1358afdd142a0
dhwang21 <hwang.derrick@gmail.com>
2018-09-28 00:27:04 -0700

    first button working

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,24 @@
+
+/*
+ * 
+ * * Details Toggle View
+ *  Book Array
+ * 
+ */
+
 let maxResultLimit = 10;
 let bookNumberArray = [];
 
 for (let i = 0; i < maxResultLimit; i++) {
   bookNumberArray.push(i);
 }
-const showHide = number => {
-  let x = document.getElementById(`form-toggle${number}`);
-  if (x.style.display === 'block') {
-    x.style.display = 'none';
-  } else {
-    x.style.display = 'block';
-    x.style.position= 'fixed';
-    
-  }
-};
+
+/*
+ *
+ * * Detailes Toggle View
+ * Hide/Show Functionaility
+ * 
+ */
 
 bookNumberArray.forEach(number => {
   let detail = document.getElementById(`detail-toggle${number}`);
@@ -26,40 +31,36 @@ bookNumberArray.forEach(number => {
   }
 });
 
-// const showHideDetail = number => {
+const showHideForm = number => {
+  let x = document.getElementById(`form-toggle${number}`);
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';    
+  }
+};
+
+
+const showHideDetail = number => {
   // let x = document.getElementById(`detail-toggle${number}`);
-  // let y = document.getElementById(`.main`);
   // if (x.style.display === 'block') {
   //   x.style.display = 'none';
   // } else {
   //   x.style.display = 'block';
-  //   x.style.position= 'fixed';
-  //   x.style.backgroundColor = '#D8C3A5';
-  //   x.style.minheight = "25vh";
-  //   x.style.minwidth = "32vw";
-  //   x.style.borderWidth = "5px";
-  //   x.style.borderStyle = "solid";
-  //   x.style.borderColor ="#E98074";
+    // x.style.position= 'fixed';
+    // x.style.backgroundColor = '#D8C3A5';
+    // x.style.minheight = "25vh";
+    // x.style.minwidth = "32vw";
+    // x.style.borderWidth = "5px";
+    // x.style.borderStyle = "solid";
+    // x.style.borderColor ="#E98074";
     // y.style.background= rgba(100,100,100,0.6);
-  //}
+  }
   
-// };
-
-$(document).ready(function(){
-
-  $(".show").click(function() {
-      $(this).parents("book-card").removeClass("hide_description");
-  });
-  $(".hide").click(function() {
-      $(this).parents(".book-card").addClass("hide_description");
-  });
-
-});
-
 
 
 const hideSubmittedBooks = number => {
   let x = document.getElementById(`display-block-${number}`);
-  console.log(x);
   x.style.display = 'none';
 };
+
